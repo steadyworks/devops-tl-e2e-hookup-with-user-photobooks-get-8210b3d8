30b13bd740074a57aecd4adeab2bbcd106d60ba1
launchpad <launchpad@steadyworks.ai>
2026-04-24 20:31:59 +0000

    base

---
+++ backend/lib/redis/client.py
@@ -0,0 +1,23 @@
+from redis.asyncio import ConnectionPool, Redis
+from redis.backoff import ExponentialBackoff
+from redis.retry import Retry
+
+from backend.env_loader import EnvLoader
+
+
+class RedisClient:
+    def __init__(self) -> None:
+        self.__connection_pool = ConnectionPool(
+            host=EnvLoader.get("REDIS_HOST"),
+            port=int(EnvLoader.get("REDIS_PORT")),
+            username=EnvLoader.get("REDIS_USERNAME"),
+            password=EnvLoader.get("REDIS_PASSWORD"),
+            decode_responses=True,
+            socket_keepalive=True,
+            health_check_interval=30,  # pings periodically to detect dead conns
+            retry=Retry(ExponentialBackoff(), retries=5),  # retries on disconnect
+        )
+        self.client = Redis(
+            connection_pool=self.__connection_pool,
+            decode_responses=True,
+        )
